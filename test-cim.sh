d55a3d1ce85c17e80cbac2e363e4307757c23b0e
Dominique Bereziat <Dominique.Bereziat@inria.fr>
2015-06-03 17:05:18 +0200

    adding test on cim

---
+++ test-cim.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "Unit tests using $(inrinfo | head -1)"
+echo "==="
+echo ""
+
+case $(inrinfo | head -1) in
+    Inrimage*) ext=inr;;
+    Heimdali*) ext=hd5;;
+esac
+
+testCim1o() {
+    echo  1 2 3 4 5 6 | cim -x 3 -y 2 > /tmp/t$$.$ext
+    x=$(par /tmp/t$$.$ext -x -y -z -v -f -o | tr -s ' ' | tr '\t' ' ')
+    assertEquals "Issue #48" "$x" " -x 3 -y 2 -v 1 -z 1 -o 1 -f"
+    rm /tmp/t$$.$ext
+}
+
+testCim4o() {
+    echo  1 2 3 4 5 6 | cim -x 3 -y 2 -r > /tmp/t$$.$ext
+    x=$(par /tmp/t$$.$ext -x -y -v -z -f -o | tr -s ' ' | tr '\t' ' ')
+    assertEquals "$x" " -x 3 -y 2 -v 1 -z 1 -o 4 -r"
+    rm /tmp/t$$.$ext
+}
+
+. shunit2/src/shunit2
